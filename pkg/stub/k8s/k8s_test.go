cee328d5066fe2ce614a80fe09ca61381e276034
André Carvalho <asantostc@gmail.com>
2018-06-26 18:19:01 -0300

    adds test for multiple replicas

--- pkg/stub/k8s/k8s_test.go
+++ pkg/stub/k8s/k8s_test.go
@@ -12,69 +12,92 @@ import (
 )
 
 func Test_NewDeployment(t *testing.T) {
+	baseNginx := v1alpha1.Nginx{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-nginx",
+			Namespace: "default",
+		},
+		Spec:   v1alpha1.NginxSpec{},
+		Status: v1alpha1.NginxStatus{},
+	}
+	baseDeploy := appv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-nginx-deployment",
+			Namespace: "default",
+		},
+		Spec: appv1.DeploymentSpec{
+			Replicas: nil,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"nginx": "my-nginx",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "",
+					Namespace: "default",
+					Labels: map[string]string{
+						"nginx": "my-nginx",
+					},
+					OwnerReferences: nil,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:    "busybox",
+							Image:   "busybox",
+							Command: []string{"sleep", "3600"},
+						},
+					},
+					RestartPolicy: "",
+				},
+			},
+		},
+	}
 	tests := []struct {
-		name string
-		n    *v1alpha1.Nginx
-		want *appv1.Deployment
+		name     string
+		nginxFn  func(n v1alpha1.Nginx) v1alpha1.Nginx
+		deployFn func(d appv1.Deployment) appv1.Deployment
 	}{
 		{
 			name: "empty-spec",
-			n: &v1alpha1.Nginx{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "my-nginx",
-					Namespace: "default",
-				},
-				Spec:   v1alpha1.NginxSpec{},
-				Status: v1alpha1.NginxStatus{},
-			}, want: &appv1.Deployment{
-				TypeMeta: metav1.TypeMeta{
-					Kind:       "Deployment",
-					APIVersion: "apps/v1",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "my-nginx-deployment",
-					Namespace: "default",
-				},
-				Spec: appv1.DeploymentSpec{
-					Replicas: nil,
-					Selector: &metav1.LabelSelector{
-						MatchLabels: map[string]string{
-							"nginx": "my-nginx",
-						},
-					},
-					Template: corev1.PodTemplateSpec{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      "",
-							Namespace: "default",
-							Labels: map[string]string{
-								"nginx": "my-nginx",
-							},
-							OwnerReferences: nil,
-						},
-						Spec: corev1.PodSpec{
-							Containers: []corev1.Container{
-								{
-									Name:    "busybox",
-									Image:   "busybox",
-									Command: []string{"sleep", "3600"},
-								},
-							},
-							RestartPolicy: "",
-						},
-					},
-				},
-			}},
+			nginxFn: func(n v1alpha1.Nginx) v1alpha1.Nginx {
+				return n
+			},
+			deployFn: func(d appv1.Deployment) appv1.Deployment {
+				return d
+			},
+		},
+		{
+			name: "multiple-replicas",
+			nginxFn: func(n v1alpha1.Nginx) v1alpha1.Nginx {
+				v := int32(3)
+				n.Spec.Replicas = &v
+				return n
+			},
+			deployFn: func(d appv1.Deployment) appv1.Deployment {
+				v := int32(3)
+				d.Spec.Replicas = &v
+				return d
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.want.OwnerReferences = []metav1.OwnerReference{
-				*metav1.NewControllerRef(tt.n, schema.GroupVersionKind{
+			nginx := tt.nginxFn(baseNginx)
+			want := tt.deployFn(baseDeploy)
+			want.OwnerReferences = []metav1.OwnerReference{
+				*metav1.NewControllerRef(&nginx, schema.GroupVersionKind{
 					Group:   v1alpha1.SchemeGroupVersion.Group,
 					Version: v1alpha1.SchemeGroupVersion.Version,
 					Kind:    "Nginx",
 				}),
 			}
-			assertDeployment(t, tt.want, NewDeployment(tt.n))
+			assertDeployment(t, &want, NewDeployment(&nginx))
 		})
 	}
 }
