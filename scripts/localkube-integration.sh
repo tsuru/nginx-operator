3381a97c9a9aff80ceeb09d57c921d705e0a5193
André Carvalho <asantostc@gmail.com>
2018-07-05 16:55:40 -0300

    rework integration

--- scripts/localkube-integration.sh
+++ scripts/localkube-integration.sh
@@ -1,19 +1,7 @@
 #!/bin/bash -xe
 
-# Download kubectl, which is a requirement for using minikube.
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-# Download minikube.
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-
-mkdir -p $HOME/.kube
-touch $HOME/.kube/config
-
-minikube start --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION
-# Fix the kubectl context, as it's often stale.
-minikube update-context
-# Wait for Kubernetes to be up and ready.
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
 make build
 kubectl apply -f deploy/ --namespace nginx-operator-integration
-sleep 5s
+sleep 10s
+
 NGINX_OPERATOR_INTEGRATION=1 go test ./...
