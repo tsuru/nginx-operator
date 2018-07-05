ecfecbd06df1d48384926aea5fb9b13b33ea100d
André Carvalho <asantostc@gmail.com>
2018-07-05 16:13:07 -0300

    sets minikube envs

--- scripts/localkube-integration.sh
+++ scripts/localkube-integration.sh
@@ -4,6 +4,10 @@
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 # Download minikube.
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+mkdir -p $HOME/.kube
+touch $HOME/.kube/config
+
 sudo minikube start --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION
 # Fix the kubectl context, as it's often stale.
 minikube update-context
