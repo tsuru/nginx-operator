467c4a5e93516350bafceea9aa64ef25e6e52c93
André Carvalho <asantostc@gmail.com>
2018-07-06 09:57:18 -0300

    disable pulling image in travis

--- scripts/localkube-integration.sh
+++ scripts/localkube-integration.sh
@@ -2,6 +2,7 @@
 
 make build
 kubectl create namespace nginx-operator-integration
+sed -ie 's/imagePullPolicy: Always/imagePullPolicy: Never/g' deploy/operator.yaml
 kubectl apply -f deploy/ --namespace nginx-operator-integration
 sleep 30s
 
