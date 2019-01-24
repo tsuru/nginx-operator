dc1cfc2ee66c3b9a0a10e0f12a8eff181419d49d
André Carvalho <asantostc@gmail.com>
2019-01-24 16:02:22 -0200

    migrates operator-sdk to 0.4.0 (#3)

--- pkg/apis/nginx/v1alpha1/doc.go
+++ pkg/apis/nginx/v1alpha1/doc.go
@@ -1,3 +1,4 @@
-// +k8s:deepcopy-gen=package
+// Package v1alpha1 contains API Schema definitions for the nginx v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
 // +groupName=nginx.tsuru.io
 package v1alpha1
