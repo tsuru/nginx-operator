0aa465f7b71037d6ed17fe0e10cca9a622c5d4b3
Claudio Netto <nettinhorama@gmail.com>
2019-05-30 18:51:07 -0300

    add integration test for a single instance w/ many certificates

--- test/integration_test.go
+++ test/integration_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tsuru/nginx-operator/pkg/apis/nginx/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -24,6 +25,7 @@ func TestMain(m *testing.M) {
 	}
 	os.Exit(m.Run())
 }
+
 func Test_Operator(t *testing.T) {
 	cleanup, err := createNamespace(testingNamespace)
 	if err != nil {
@@ -42,6 +44,60 @@ func Test_Operator(t *testing.T) {
 		assert.Equal(t, 2, len(nginx.Status.Pods))
 		assert.Equal(t, 1, len(nginx.Status.Services))
 	})
+
+	t.Run("with-certificates.yaml", func(t *testing.T) {
+		err = apply("testdata/with-certificates.yaml", testingNamespace)
+		require.NoError(t, err)
+
+		nginx, err := getReadyNginx("my-secured-nginx", 1, 1)
+		require.NoError(t, err)
+		require.NotNil(t, nginx)
+		assert.Equal(t, "nginx:alpine", nginx.Spec.Image)
+
+		defer func() {
+			err = delete("testdata/with-certificates.yaml", testingNamespace)
+			require.NoError(t, err)
+		}()
+
+		nginxService := corev1.Service{TypeMeta: metav1.TypeMeta{APIVersion: "v1", Kind: "Service"}}
+		err = get(&nginxService, "my-secured-nginx-service", nginx.Namespace)
+		require.NoError(t, err)
+		assert.NotNil(t, nginxService)
+		assert.Equal(t, int32(80), nginxService.Spec.Ports[0].Port)
+		assert.Equal(t, int32(443), nginxService.Spec.Ports[1].Port)
+
+		podName := nginx.Status.Pods[0].Name
+		err = waitPodBeAvailable(podName, testingNamespace)
+		require.NoError(t, err)
+
+		tests := []struct{
+			filename string
+			expectedSha256 string
+		}{
+			{
+				filename: "/etc/nginx/certs/rsa.crt",
+				expectedSha256: "f50457089e715bbc9d5a31a16cf53cc2f13a68333df71559bb5d06be2d2b8a63",
+			},
+			{
+				filename: "/etc/nginx/certs/rsa.key",
+				expectedSha256: "18580c25b2807b4c95502dd7051d414299e40d8d14024ad5d69c9915ec41e66e",
+			},
+			{
+				filename: "/etc/nginx/certs/custom_dir/custom_name.crt",
+				expectedSha256: "159af275ab3b22d9737617e51daca64efafb48287ecb3650661d2116cb4ef0c9",
+			},
+			{
+				filename: "/etc/nginx/certs/custom_dir/custom_name.key",
+				expectedSha256: "253b9795dcd80c493dcfade6b3dc5506fac1a38850abaa4e639fada5ea3dad5e",
+			},
+		}
+
+		for _, tt := range tests {
+			output, err := kubectl("exec", podName, "-n", testingNamespace, "--", "sha256sum", tt.filename)
+			require.NoError(t, err)
+			assert.Contains(t, string(output), tt.expectedSha256)
+		}
+	})
 }
 
 func getReadyNginx(name string, expectedPods int, expectedSvcs int) (*v1alpha1.Nginx, error) {
@@ -59,6 +115,22 @@ func getReadyNginx(name string, expectedPods int, expectedSvcs int) (*v1alpha1.N
 		case <-timeout:
 			return nil, fmt.Errorf("Timeout waiting for nginx status. Last nginx object: %#v. Last error: %v", nginx, err)
 		case <-time.After(time.Millisecond * 100):
+		}
+	}
+}
+
+func waitPodBeAvailable(name, namespace string) error {
+	timeout := time.After(5 * time.Minute)
+	pingMessage := "PING"
+	for {
+		output, err := kubectl("exec", name, "-n", namespace, "--", "echo", "-n", pingMessage)
+		if err == nil && string(output) == pingMessage {
+			return nil
+		}
+		select {
+		case <-timeout:
+			return fmt.Errorf("Timeout waiting pod %q becomes available. Last output: %s. Last error: %v", name, string(output), err)
+		case <-time.After(100 * time.Millisecond):
 		}
 	}
 }
