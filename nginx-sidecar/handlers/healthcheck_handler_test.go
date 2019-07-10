b1b40dc4d22cae343823d181b6abfbcda0d69744
Bernardo Lins <bernardolins28@gmail.com>
2019-07-10 18:09:45 -0300

    Use best asserts on healthcheck_handler tests

--- nginx-sidecar/handlers/healthcheck_handler_test.go
+++ nginx-sidecar/handlers/healthcheck_handler_test.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -66,18 +68,13 @@ func TestHealthcheckHandler(t *testing.T) {
 func testHandler(t *testing.T, handler func(http.ResponseWriter, *http.Request), testCases []handlerTestCase) {
 	for _, testCase := range testCases {
 		req, err := http.NewRequest("GET", testCase.query, nil)
-
-		if err != nil {
-			t.Fatal(err)
-		}
+		require.NoError(t, err)
 
 		recorder := httptest.NewRecorder()
 		handler := http.HandlerFunc(handler)
 
 		handler.ServeHTTP(recorder, req)
 
-		if status := recorder.Code; status != testCase.expected {
-			t.Errorf("%v: expected status code %v got %v", testCase.name, testCase.expected, status)
-		}
+		assert.Equal(t, testCase.expected, recorder.Code)
 	}
 }
