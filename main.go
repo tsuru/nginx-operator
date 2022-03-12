4544bb8929458a633e072fe458ecdfbd510ee267
Claudio Netto <nettinhorama@gmail.com>
2022-03-12 01:12:00 -0300

    chore: update project layout to kubebuilder/v3

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ package main
 
 import (
 	"flag"
-	"net/http"
 	"os"
 	"time"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -16,8 +19,8 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	ctrlzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	nginxv1alpha1 "github.com/tsuru/nginx-operator/api/v1alpha1"
@@ -26,18 +29,24 @@ import (
 )
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	scheme = runtime.NewScheme()
 
-	metricsAddr             = flag.String("metrics-addr", ":8080", "The TCP address that controller should bind to for serving Prometheus metrics. It can be set to \"0\" to disable the metrics serving.")
-	healthAddr              = flag.String("health-addr", ":8081", "The TCP address that controller should bind to for serving health probes.")
-	enableLeaderElection    = flag.Bool("enable-leader-election", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
-	leaderElectionNamespace = flag.String("leader-election-namespace", "", "Namespace where the leader election object will be created.")
-	syncPeriod              = flag.Duration("reconcile-sync", 10*time.Hour, "Resync frequency of Nginx resources.")
-	logFormat               = flag.String("log-format", "json", "Set the format of logging (options: json, console)")
-	logLevel                = zap.LevelFlag("log-level", zapcore.InfoLevel, "Set the level of logging (options: debug, info, warn, error, dpanic, panic, fatal)")
-	namespace               = flag.String("namespace", "", "Limit the observed Nginxes to a specific namespace (empty means all namespaces)")
-	annotationFilter        = flag.String("annotation-filter", "", "Filter Nginx resources via annotation using label selector semantics (default: all Nginx resources)")
+	// Following the standard of flags on Kubernetes.
+	// See more: https://github.com/kubernetes-sigs/kubebuilder/issues/1839
+	metricsAddr = flag.String("metrics-bind-address", ":8080", "The TCP address that controller should bind to for serving Prometheus metrics. It can be set to \"0\" to disable the metrics serving.")
+	healthAddr  = flag.String("health-probe-bind-address", ":8081", "The TCP address that controller should bind to for serving health probes.")
+
+	leaderElection                  = flag.Bool("leader-elect", true, "Start a leader election client and gain leadership before executing the main loop. Enable this when running replicated components for high availability.")
+	leaderElectionResourceName      = flag.String("leader-elect-resource-name", "nginx-operator-lock", "The name of resource object that is used for locking during leader election.")
+	leaderElectionResourceNamespace = flag.String("leader-elect-resource-namespace", "", "The namespace of resource object that is used for locking during leader election.")
+
+	syncPeriod = flag.Duration("sync-period", 10*time.Hour, "The resync period for reconciling manager resources.")
+
+	logFormat = flag.String("log-format", "json", "Set the format of logging (options: json, console)")
+	logLevel  = zap.LevelFlag("log-level", zapcore.InfoLevel, "Set the level of logging (options: debug, info, warn, error, dpanic, panic, fatal)")
+
+	namespace        = flag.String("namespace", "", "Limit the observed Nginx resources from specific namespace (empty means all namespaces)")
+	annotationFilter = flag.String("annotation-filter", "", "Filter Nginx resources via annotation using label selector semantics (default: all Nginx resources)")
 )
 
 func init() {
@@ -61,50 +70,59 @@ func main() {
 	))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                  scheme,
-		MetricsBindAddress:      *metricsAddr,
-		Namespace:               *namespace,
-		LeaderElection:          *enableLeaderElection,
-		LeaderElectionID:        "nginx-operator-lock",
-		LeaderElectionNamespace: *leaderElectionNamespace,
-		SyncPeriod:              syncPeriod,
-		HealthProbeBindAddress:  *healthAddr,
+		Scheme:                     scheme,
+		MetricsBindAddress:         *metricsAddr,
+		Namespace:                  *namespace,
+		LeaderElectionResourceLock: "leases",
+		LeaderElection:             *leaderElection,
+		LeaderElectionID:           *leaderElectionResourceName,
+		LeaderElectionNamespace:    *leaderElectionResourceNamespace,
+		SyncPeriod:                 syncPeriod,
+		HealthProbeBindAddress:     *healthAddr,
 	})
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
+		ctrl.Log.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
 
-	// NOTE: registering a dummy checker just to activate the /healthz endpoint
-	mgr.AddHealthzCheck("", func(_ *http.Request) error { return nil })
-
 	ls, err := metav1.ParseToLabelSelector(*annotationFilter)
 	if err != nil {
-		setupLog.Error(err, "unable to convert annotation filter to label selector")
+		ctrl.Log.Error(err, "unable to convert annotation filter to label selector")
 		os.Exit(1)
 	}
 
 	annotationSelector, err := metav1.LabelSelectorAsSelector(ls)
 	if err != nil {
-		setupLog.Error(err, "unable to convert annotation filter to selector")
+		ctrl.Log.Error(err, "unable to convert annotation filter to selector")
 		os.Exit(1)
 	}
 
-	r := &controllers.NginxReconciler{
+	err = (&controllers.NginxReconciler{
 		Client:           mgr.GetClient(),
 		Log:              ctrl.Log.WithName("controllers").WithName("Nginx"),
 		Scheme:           mgr.GetScheme(),
 		AnnotationFilter: annotationSelector,
-	}
-	if err = r.SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Nginx")
+	}).SetupWithManager(mgr)
+	if err != nil {
+		ctrl.Log.Error(err, "unable to create controller", "controller", "Nginx")
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
 
-	setupLog.Info("starting manager")
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		ctrl.Log.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		ctrl.Log.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	ctrl.Log.Info("starting manager")
+
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
+		ctrl.Log.Error(err, "problem running manager")
 		os.Exit(1)
 	}
 }
